db5955f6bd82686accd26229fa0136eb81e706c0
Christian G. Warden <cwarden@xerus.org>
2014-07-25 11:55:08 -0700

    Wire up zoom, latitude, and longitude inputs
    Bind to input events on Wolf model, and update the zoom, latitude, and
    longitude attributes when the values change.
    
    Set up bindings from Wolf model's zoom, latitude, and longitude
    attributes to the rendered inputs.

--- clientapp/views/wolf.js
+++ clientapp/views/wolf.js
@@ -4,6 +4,9 @@ var WolfTemplate = require('../templates/wolf.jade');
 module.exports = View.extend({
     template: WolfTemplate,
     autoRender: true,
+    events: {
+        'input input[role]': 'changePropByRole'
+    },
     bindings: {
         'model.title': {
             role: 'name'
@@ -12,6 +15,25 @@ module.exports = View.extend({
             type: 'attribute',
             name: 'src',
             role: 'map'
-        }
+        },
+        'model.zoom': {
+            type: 'value',
+            role: 'zoom'
+        },
+        'model.latitude': {
+            type: 'value',
+            role: 'latitude'
+        },
+        'model.longitude': {
+            type: 'value',
+            role: 'longitude'
+        },
+    },
+
+    changePropByRole: function(event) {
+        var role = event.target.getAttribute('role');
+        var value = event.target.value;
+
+        this.model[role] = parseFloat(value);
     }
 });
