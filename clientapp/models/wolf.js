cd529ae1e8adb99499cd9f2053b30e79e1e0236d
Christian G. Warden <cwarden@xerus.org>
2014-07-25 11:39:46 -0700

    Display Google map in Wolf view

--- clientapp/models/wolf.js
+++ clientapp/models/wolf.js
@@ -1,4 +1,5 @@
 var Model = require('ampersand-model');
+var querystring = require('querystring');
 
 module.exports = Model.extend({
     props: {
@@ -9,13 +10,31 @@ module.exports = Model.extend({
         longitude: 'number'
     },
     session: {
-        map: ['string', true, 'http://placehold.it/200x200']
+        size: ['string', true, '200x200'],
+        zoom: ['number', true, 7],
+        key: ['string', true, 'AIzaSyBMMJfzDkhIDbOFR_TYDRmCWWSqQK0rapY']
     },
     derived: {
         title: {
             deps: ['name', 'age'],
             fn: function () {
                 return this.name + ' ' + this.age;
+            }
+        },
+        map: {
+            deps: ['latitude', 'longitude', 'size', 'zoom', 'key'],
+            fn: function() {
+                if (!this.latitude || !this.longitude) {
+                    return 'http://placehold.it/200x200';
+                }
+                return 'http://maps.googleapis.com/maps/api/staticmap?' +
+                    querystring.stringify({
+                        center: this.latitude + ',' + this.longitude,
+                        markers: this.latitude + ',' + this.longitude,
+                        size: this.size,
+                        zoom: this.zoom,
+                        key: this.key
+                    });
             }
         }
     }
