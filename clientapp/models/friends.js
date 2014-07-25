13be21b83c90e1e597fe7eff9103bd02329ba4ef
Christian G. Warden <cwarden@xerus.org>
2014-07-25 12:06:44 -0700

    Update friends map size when Me size changes

--- clientapp/models/friends.js
+++ clientapp/models/friends.js
@@ -5,6 +5,12 @@ module.exports = Collection.extend({
     model: Wolf,
     initialize: function () {
         this.page = 0;
+        var self = this;
+        this.parent.on('change:size', function(parent, value) {
+            self.each(function(model) {
+                model.size = value;
+            });
+        });
     },
     url: function () {
         this.page++;
